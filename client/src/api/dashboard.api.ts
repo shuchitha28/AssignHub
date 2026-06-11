78bce9ac88fb960d77ff2501505abc7484267c27
Shuchitha K M <131381603+shuchitha28@users.noreply.github.com>
2026-06-12 00:07:36 +0530

    Update dashboard.api.ts

--- client/src/api/dashboard.api.ts
+++ client/src/api/dashboard.api.ts
@@ -3,5 +3,5 @@ import API from "./axios";
 export const getDashboard = () => API.get("/dashboard");
 export const getStudentDashboard = () =>
   API.get("/dashboard/student");
-export const getTeacherDashboard = () =>
+export const getTeacherDashboard = (range) =>
   API.get("/dashboard/teacher");
